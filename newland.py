7dbe9a8c2043ffeb87e352984249b8c64b1a0826
Kristi <Krispy2009@gmail.com>
2015-04-21 10:02:18 +0100

    Setting correct chunk size for newland scanner

--- newland.py
+++ newland.py
@@ -3,14 +3,13 @@ from keyboard_alike import reader
 
 class BarCodeReader(reader.Reader):
     """
-    This class supports Lindy USB bar code scanner configured to work as a keyboard
-    http://www.lindy.co.uk/accessories-c9/input-devices-c357/barcode-scanners-c360/barcode-scanner-ccd-usb-p1352
+    This class supports Newland 2D Barcode Scanner (NLS-HR32)
     """
     pass
 
 
 if __name__ == "__main__":
-    reader = BarCodeReader(0x1eab, 0x0003, 84, 6, should_reset=True)
+    reader = BarCodeReader(0x1eab, 0x0003, 84, 8, should_reset=True)
     reader.initialize()
     print(reader.read().strip())
     reader.disconnect()
